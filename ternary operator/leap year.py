d6edf6806099d6460448bfe7599645cb72342e7b
Chandresh-01 <chandreshjangra10@gmail.com>
2025-08-14 16:52:00 +0530

    leap year.py

--- ternary operator/leap year.py
+++ ternary operator/leap year.py
@@ -1,3 +1,3 @@
 year = int(input("Enter a year: "))
-print("Leap Year" if (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)) else "Not a Leap Year")
+print("It's is a Leap Year" if (year % 400 == 0 or (year % 4 == 0 and year % 100 != 0)) else "It's not a Leap Year")
 
